ed062f62cce9236da38d1af0a5bb0cad1b891096
CrazyCat7100 <134160263+CrazyCat7100@users.noreply.github.com>
2023-11-11 04:08:26 +0000

    .

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let options = document.getElementsByTagName('options')[0]
+let toggleOptions1 = document.getElementsByClassName('toggleOptions1')[0]
+let toggleOptions2 = document.getElementsByClassName('toggleOptions2')[0]
+let options = document.getElementsByClassName('options')[0]
+let header = document.getElementsByTagName('header')[0]
 
-options.addEventListener('click', function () {
-    
+toggleOptions1.addEventListener('click', function () {
+    options.style.display = 'flex'
+    header.style.display = 'none'
+})
+
+toggleOptions2.addEventListener('click', function () {
+    options.style.display = 'none'
+    header.style.display = flex
 })
